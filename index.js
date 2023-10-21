77544a7aa53e172cba890f93796ac287bb5e651a
George Huang <gzh@princeton.alumni.com>
2023-10-20 20:29:58 -0700

    auto generate and pass in token, artist not found resolved

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
+const simArtistContainer = document.querySelector(".sim-art-container");
+const submitBtn = document.querySelector("#submit-btn");
 
-  
-const simArtistContainer = document.querySelector('.sim-art-container');
-const submitBtn = document.querySelector('#submit-btn');
-
-const artistInput = document.querySelector('#form-input');
+const artistInput = document.querySelector("#form-input");
 
 // let link = `https://api.spotify.com/v1/search?q=${artistInput}&type=artist`
 
@@ -18,107 +16,107 @@ const artistInput = document.querySelector('#form-input');
 //   .then(data => console.log(data))
 //   .catch(error => console.error("Error:", error))
 
-
 let artistID = "";
+let token;
+
+fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: "grant_type=client_credentials&client_id=7cd68b30da0c423dabba304bc46ac8ea&client_secret=e3903bf524744be4835f818adf5f64bc",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+        token = data.access_token;
+        console.log(token);
+    })
+    .catch((error) => console.error("Error:", error));
 
 async function bigBoi() {
+  console.log(artistInput);
 
-console.log(artistInput)
+  const response = document.createElement("p");
+  response.classList.add("response-text");
+  response.innerText = "Check out these similar artists";
+  simArtistContainer.appendChild(response);
 
-  const response = document.createElement('p');
-    response.classList.add('response-text')
-    response.innerText = 'Check out these similar artists'
-    simArtistContainer.appendChild(response);
-
-
-  fetch("https://accounts.spotify.com/api/token", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/x-www-form-urlencoded"
-  },
-  body: "grant_type=client_credentials&client_id=7cd68b30da0c423dabba304bc46ac8ea&client_secret=e3903bf524744be4835f818adf5f64bc"
-})
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error("Error:", error));
-
-
-    async function spotSearch() {
+  async function spotSearch() {
     //change this url later to our link variable
-        const url = `https://api.spotify.com/v1/search?q=${artistInput.value}&type=artist`;
-        const options = {
-            method: 'GET',
-            headers: {
-                'Authorization': 'Bearer  BQBsYlS1LqE3lDRq0mCiyIgvCKSradjW1U9EjUvAPmkSUX_3jnOEJx3nupc_ACRorxbFQsUDI7L3WqF3jStkXenjOBoIArIRm4J8NS-J1AwXDNf5DMY'
-                // 'X-RapidAPI-Key': '722509c4c2mshb2186d787c1df4ep1431e5jsne472d0a1e0a7',
-                // 'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
-            }
-        };
-        
-        try {
-            const response = await fetch(url, options);
-            const result = await response.json();
-            // console.log(result);
-            // console.log(result.artists.items[0].id);
-            artistID = "";
-            artistID += result.artists.items[0].id;
-        } catch (error) {
-            console.error(error);
-        }
+    const url = `https://api.spotify.com/v1/search?q=${artistInput.value}&type=artist`;
+    const options = {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer  ${token}`
+      },
+    };
+
+    try {
+      const response = await fetch(url, options);
+      const result = await response.json();
+      // console.log(result);
+      // console.log(result.artists.items[0].id);
+      artistID = "";
+      artistID += result.artists.items[0].id;
+    } catch (error) {
+      console.error(error);
+      console.log('Search ID error')
+      const noArtistFound = document.createElement("p");
+      noArtistFound.classList.add("sim-artist");
+      noArtistFound.innerText = "Artist could not be found";
+      simArtistContainer.appendChild(noArtistFound);
     }
+  }
 
-    await spotSearch()
+  await spotSearch();
 
-    console.log(artistID)
+  console.log(artistID);
 
-    async function relateSearch() {
+  async function relateSearch() {
     //change this url later to our link variable
-        const url = `https://api.spotify.com/v1/artists/${artistID}/related-artists`;
-        const options = {
-            method: 'GET',
-            headers: {
-                'Authorization': 'Bearer  BQBsYlS1LqE3lDRq0mCiyIgvCKSradjW1U9EjUvAPmkSUX_3jnOEJx3nupc_ACRorxbFQsUDI7L3WqF3jStkXenjOBoIArIRm4J8NS-J1AwXDNf5DMY'
-            }
-        };
-        
-        try {
-            const response = await fetch(url, options);
-            const result = await response.json();
-            // console.log(result);
-            for (let i = 0; i <= 5; i++){
-            
-            //create and append to DOM
-            const name = document.createElement('p');
-            name.classList.add('sim-artist')
-            name.innerText = result.artists[i].name
-            simArtistContainer.appendChild(name);
-            // console.log(result.artists[i].name)
-            
-            // create and append to DOM
-            const url = document.createElement('a');
-            url.classList.add('sim-url')
-            url.setAttribute('href', result.artists[i].external_urls.spotify);
-            url.setAttribute('target', '_blank')
-            url.innerText = 'Listen Here';
-            simArtistContainer.appendChild(url);
-            // console.log(result.artists[i].external_urls.spotify)
-            }
+    const url = `https://api.spotify.com/v1/artists/${artistID}/related-artists`;
+    const options = {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer  ${token}`
+      },
+    };
 
-        } catch (error) {
-            console.error(error);
-        }
+    try {
+      const response = await fetch(url, options);
+      const result = await response.json();
+      // console.log(result);
+      for (let i = 0; i <= 5 && result.artists[i] !== undefined; i++) { // second condition in case there aren't enough similar artists
+        //create and append to DOM
+        const name = document.createElement("p");
+        name.classList.add("sim-artist");
+        name.innerText = result.artists[i].name;
+        simArtistContainer.appendChild(name);
+        // console.log(result.artists[i].name)
+
+        // create and append to DOM
+        const url = document.createElement("a");
+        url.classList.add("sim-url");
+        url.setAttribute("href", result.artists[i].external_urls.spotify);
+        url.setAttribute("target", "_blank");
+        url.innerText = "Listen Here";
+        simArtistContainer.appendChild(url);
+        // console.log(result.artists[i].external_urls.spotify)
+      }
+    } catch (error) {
+      console.error(error);
+      console.log('Similar artists error')
     }
+  }
 
-    await relateSearch()
-
+  await relateSearch();
 }
 
-submitBtn.addEventListener('click', function(e){
+submitBtn.addEventListener("click", function (e) {
   e.preventDefault();
-  while (simArtistContainer.firstChild){
+  while (simArtistContainer.firstChild) {
     simArtistContainer.removeChild(simArtistContainer.lastChild);
   }
-  submitBtn.style.marginBottom = '';
+  submitBtn.style.marginBottom = "";
   bigBoi();
-})
-
+});
